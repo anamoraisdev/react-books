613f07e538e906ef9aedd5100bfaf571eb03438d
anamoraisdev <anamorais.dev@gmail.com>
2023-04-19 12:49:14 -0300

    renderizando e estilizando os pedidos

--- src/paginas/paginaLogado/pedidos.tsx
+++ src/paginas/paginaLogado/pedidos.tsx
@@ -19,7 +19,17 @@ const PedidosUsuario = () => {
     return(
         <section className="pedidos">
             <h1>Meus pedidos</h1>
-            
+            {listaPedidos && listaPedidos.map((pedido) => 
+                <div className="pedido" key={pedido.id}>
+                 <ul>
+                    <li>pedido:<strong>{pedido.id}</strong></li>
+                    <li>data do pedido:<strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+                    <li>valor total:<strong>{Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(pedido.total)}</strong></li>
+                    <li>entrega realizada em:<strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+                 </ul>
+                 <AbBotao texto="Detalhes"/>
+                </div>
+            )}
         </section>
     )
 }
